bf5af6e5aa3d306298d8f8bfbef13a62d3dc5a95
Josh Hull <josh.hull@lypht.com>
2017-07-30 07:46:10 -0500

    first test of kex

--- utils.bash
+++ utils.bash
@@ -1,3 +1,27 @@
-get_pods() {
-  kubectl get pods -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}'
+pod_def(){
+  kubectl get pods -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}' | nl
+}
+
+pod_default(){
+  kubectl get pods -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}' | sed -n 1p
+}
+
+kube_def_bash(){
+  kubectl exec -it $(pod_default) bash
+}
+
+kube_def_sh(){
+  kubectl exec -it $(pod_default) sh
+}
+
+pod_select(){
+  kubectl get pods -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}' | sed -n ${1}p
+}
+
+kube_exec_sh() {
+  kubectl exec -it $(pod_select) sh
+}
+
+kube_exec_bash() {
+  kubectl exec -it $(pod_select) bash
 }
